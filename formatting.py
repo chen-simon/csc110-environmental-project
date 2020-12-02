84f4649017c7d9d0bfbaa17c1b310c32e9a5afb2
narwhalblast <ognomisch@gmail.com>
2020-12-02 17:26:27 -0500

    Update formatting.py

--- formatting.py
+++ formatting.py
@@ -1,1 +1,13 @@
 import xlrd
+import csv
+from typing import List, Any
+
+def xlsx_to_data(filename: str) -> List[List[Any]]:
+    """ Convert the xlsx file to usable data.
+    """
+    # TODO: Implement your function here.
+
+def csv_to_data(filename: str) -> List[List[Any]]:
+    """ Convert the csv file to usable data.
+    """
+    # TODO: Implement your function here.
