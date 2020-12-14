ab6bf2631e320b2ec5a45ab4f8e439c99aadc050
narwhalblast <ognomisch@gmail.com>
2020-12-13 19:49:52 -0500

    Update interface.py
    Removed all top level statements to be in functions

--- interface.py
+++ interface.py
@@ -1,87 +1,16 @@
 import pygame
 import pygame_gui
 from main import run
+from typing import List
 pygame.init()
 
-pygame.display.set_caption('Climate Grapher')
-window_surface = pygame.display.set_mode((450, 300))
 
-background = pygame.Surface((450, 300))
-background.fill(pygame.Color('#CCCCCC'))
-
-manager = pygame_gui.UIManager((450, 300))
-
-# Dataset Buttons
-temperature_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 25), (150, 50)),
-                                                  text='Temperature',
-                                                  manager=manager)
-
-disasters_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 85), (150, 50)),
-                                                text='Natural Disasters',
-                                                manager=manager)
-
-co2_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 145), (150, 50)),
-                                          text='CO2 levels',
-                                          manager=manager)
-
-# Increase Boxes
-temperature_increase = pygame_gui.elements.UITextBox(
-    relative_rect=pygame.Rect((225, 25), (200, 50)),
-    html_text='+0.1°C',
-    manager=manager)
-
-disasters_increase = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((225, 85), (200, 50)),
-                                                   html_text='',
-                                                   manager=manager)
-
-co2_increase = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((225, 145), (200, 50)),
-                                             html_text='',
-                                             manager=manager)
-# Plus Minus Buttons
-temperature_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 25), (50, 25)),
-                                                text='+',
-                                                manager=manager)
-
-temperature_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 50), (50, 25)),
-                                                 text='-',
-                                                 manager=manager)
-
-disasters_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 85), (50, 25)),
-                                              text='+',
-                                              manager=manager)
-
-disasters_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 110), (50, 25)),
-                                               text='-',
-                                               manager=manager)
-
-co2_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 145), (50, 25)),
-                                        text='+',
-                                        manager=manager)
-
-co2_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 170), (50, 25)),
-                                         text='-',
-                                         manager=manager)
-# Generate Button
-plot_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 205), (400, 70)),
-                                           text='Generate',
-                                           manager=manager)
-
-clock = pygame.time.Clock()
-is_running = True
-
-selected = [True, False, False]
-interval_increases = [0.1, 0, 0]
-temperature_button.select()
-
-units = ['°C', ' Disasters', ' ppm']
-interval_deltas = [0.1, 20, 10]
-
-
-def toggle_dataset(button, increase_box, dataset: int):
+def toggle_dataset(interval_increases: List[float], selected: List[int],
+                   button, increase_box, dataset: int):
     if not selected[dataset]:
         selected[dataset] = True
         button.select()
-        change_interval(increase_box, 0, dataset)
+        change_interval(interval_increases, increase_box, 0, dataset)
     else:
         selected[dataset] = False
         button.unselect()
@@ -89,7 +18,8 @@ def toggle_dataset(button, increase_box, dataset: int):
         increase_box.rebuild()
 
 
-def change_interval(textbox, delta: float, dataset: int) -> None:
+def change_interval(interval_increases: List[float], textbox, delta: float, dataset: int) -> None:
+    units = ['°C', ' Disasters', ' ppm']
     interval_increases[dataset] += delta
     # Always round the Degrees Celsius
     interval_increases[0] = round(interval_increases[0], 1)
@@ -100,67 +30,136 @@ def change_interval(textbox, delta: float, dataset: int) -> None:
     textbox.rebuild()
 
 
-def deselect_datasets():
-    temperature_button.unselect()
-    disasters_button.unselect()
-    co2_button.unselect()
+def run_interface() -> None:
+    """ Runs the graphical user interface
+    """
+    pygame.display.set_caption('Climate Grapher')
+    window_surface = pygame.display.set_mode((450, 300))
 
+    background = pygame.Surface((450, 300))
+    background.fill(pygame.Color('#CCCCCC'))
 
-while is_running:
-    time_delta = clock.tick(60) / 1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+    manager = pygame_gui.UIManager((450, 300))
 
-        if event.type == pygame.USEREVENT:
-            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                # Dataset buttons
-                if event.ui_element == temperature_button:
-                    toggle_dataset(temperature_button, temperature_increase, 0)
-                elif event.ui_element == disasters_button:
-                    toggle_dataset(disasters_button, disasters_increase, 1)
-                elif event.ui_element == co2_button:
-                    toggle_dataset(co2_button, co2_increase, 2)
+    # Dataset Buttons
+    temperature_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 25), (150, 50)),
+                                                      text='Temperature',
+                                                      manager=manager)
 
-                # Plus minus interval buttons
-                elif event.ui_element == temperature_plus:
-                    change_interval(temperature_increase, interval_deltas[0], 0)
-                    if not selected[0]:
-                        toggle_dataset(temperature_button, temperature_increase, 0)
+    disasters_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 85), (150, 50)),
+                                                    text='Natural Disasters',
+                                                    manager=manager)
 
-                elif event.ui_element == temperature_minus:
-                    change_interval(temperature_increase, -interval_deltas[0], 0)
-                    if not selected[0]:
-                        toggle_dataset(temperature_button, temperature_increase, 0)
+    co2_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 145), (150, 50)),
+                                              text='CO2 levels',
+                                              manager=manager)
 
-                elif event.ui_element == disasters_plus:
-                    change_interval(disasters_increase, interval_deltas[1], 1)
-                    if not selected[1]:
-                        toggle_dataset(disasters_button, disasters_increase, 1)
-                elif event.ui_element == disasters_minus:
-                    change_interval(disasters_increase, -interval_deltas[1], 1)
-                    if not selected[1]:
-                        toggle_dataset(disasters_button, disasters_increase, 1)
+    # Increase Boxes
+    temperature_increase = pygame_gui.elements.UITextBox(
+        relative_rect=pygame.Rect((225, 25), (200, 50)),
+        html_text='+0.1°C',
+        manager=manager)
 
-                elif event.ui_element == co2_plus:
-                    change_interval(co2_increase, interval_deltas[2], 2)
-                    if not selected[2]:
-                        toggle_dataset(co2_button, co2_increase, 2)
-                elif event.ui_element == co2_minus:
-                    change_interval(co2_increase, -interval_deltas[2], 2)
-                    if not selected[2]:
-                        toggle_dataset(co2_button, co2_increase, 2)
+    disasters_increase = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((225, 85), (200, 50)),
+                                                       html_text='',
+                                                       manager=manager)
 
-                # Generate Button
-                elif event.ui_element == plot_button:
-                    run(selected[0], selected[1], selected[2], interval_increases[0], interval_increases[1],
-                        interval_increases[2])
+    co2_increase = pygame_gui.elements.UITextBox(relative_rect=pygame.Rect((225, 145), (200, 50)),
+                                                 html_text='',
+                                                 manager=manager)
+    # Plus Minus Buttons
+    temperature_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 25), (50, 25)),
+                                                    text='+',
+                                                    manager=manager)
 
-        manager.process_events(event)
+    temperature_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 50), (50, 25)),
+                                                     text='-',
+                                                     manager=manager)
 
-    manager.update(time_delta)
+    disasters_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 85), (50, 25)),
+                                                  text='+',
+                                                  manager=manager)
 
-    window_surface.blit(background, (0, 0))
-    manager.draw_ui(window_surface)
+    disasters_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 110), (50, 25)),
+                                                   text='-',
+                                                   manager=manager)
 
-    pygame.display.update()
+    co2_plus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 145), (50, 25)),
+                                            text='+',
+                                            manager=manager)
+
+    co2_minus = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((175, 170), (50, 25)),
+                                             text='-',
+                                             manager=manager)
+    # Generate Button
+    plot_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((25, 205), (400, 70)),
+                                               text='Generate',
+                                               manager=manager)
+
+    clock = pygame.time.Clock()
+    is_running = True
+
+    selected = [True, False, False]
+    interval_increases = [0.1, 0, 0]
+    temperature_button.select()
+
+    interval_deltas = [0.1, 20, 10]
+
+    while is_running:
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    # Dataset buttons
+                    if event.ui_element == temperature_button:
+                        toggle_dataset(interval_increases, selected, temperature_button, temperature_increase, 0)
+                    elif event.ui_element == disasters_button:
+                        toggle_dataset(interval_increases, selected, disasters_button, disasters_increase, 1)
+                    elif event.ui_element == co2_button:
+                        toggle_dataset(interval_increases, selected, co2_button, co2_increase, 2)
+
+                    # Plus minus interval buttons
+                    elif event.ui_element == temperature_plus:
+                        change_interval(interval_increases, temperature_increase, interval_deltas[0], 0)
+                        if not selected[0]:
+                            toggle_dataset(interval_increases, selected, temperature_button, temperature_increase, 0)
+
+                    elif event.ui_element == temperature_minus:
+                        change_interval(interval_increases, temperature_increase, -interval_deltas[0], 0)
+                        if not selected[0]:
+                            toggle_dataset(interval_increases, selected, temperature_button, temperature_increase, 0)
+
+                    elif event.ui_element == disasters_plus:
+                        change_interval(interval_increases, disasters_increase, interval_deltas[1], 1)
+                        if not selected[1]:
+                            toggle_dataset(interval_increases, selected, disasters_button, disasters_increase, 1)
+                    elif event.ui_element == disasters_minus:
+                        change_interval(interval_increases, disasters_increase, -interval_deltas[1], 1)
+                        if not selected[1]:
+                            toggle_dataset(interval_increases, selected, disasters_button, disasters_increase, 1)
+
+                    elif event.ui_element == co2_plus:
+                        change_interval(interval_increases, co2_increase, interval_deltas[2], 2)
+                        if not selected[2]:
+                            toggle_dataset(interval_increases, selected, co2_button, co2_increase, 2)
+                    elif event.ui_element == co2_minus:
+                        change_interval(interval_increases, co2_increase, -interval_deltas[2], 2)
+                        if not selected[2]:
+                            toggle_dataset(interval_increases, selected, co2_button, co2_increase, 2)
+
+                    # Generate Button
+                    elif event.ui_element == plot_button:
+                        run(selected[0], selected[1], selected[2], interval_increases[0], interval_increases[1],
+                            interval_increases[2])
+
+            manager.process_events(event)
+
+        manager.update(time_delta)
+
+        window_surface.blit(background, (0, 0))
+        manager.draw_ui(window_surface)
+
+        pygame.display.update()
